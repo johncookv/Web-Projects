329c1e620a2a7020bae6c4b8f5476d7a1330f13f
John Vehikite <johncookv@gmail.com>
2017-09-08 03:17:21 -0700

    list items can be added to todo list

--- TodoList/assets/js/todos.js
+++ TodoList/assets/js/todos.js
@@ -1,10 +1,19 @@
-$("li").on("click", function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
 
-$("span").on("click", function(e) {
-	$(this).parent().fadeOut(1000, function() {
+$("ul").on("click", "span", function(e) {
+	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	});
 	e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e) {
+	// check if user pressed enter
+	if (e.which === 13) {
+		var text = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</span> " + text + "</li>");
+	}
 })
