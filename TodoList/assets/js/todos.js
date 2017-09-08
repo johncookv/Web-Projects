8a75c951bb48e1efcbfcdb6c247d42161b3364a9
John Vehikite <johncookv@gmail.com>
2017-09-08 03:05:37 -0700

    added delete item functionality

--- TodoList/assets/js/todos.js
+++ TodoList/assets/js/todos.js
@@ -1,3 +1,10 @@
 $("li").on("click", function() {
 	$(this).toggleClass("completed");
 });
+
+$("span").on("click", function(e) {
+	$(this).parent().fadeOut(1000, function() {
+		$(this).remove();
+	});
+	e.stopPropagation();
+})
